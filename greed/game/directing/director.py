b8fae0d1dd01a18947bfdc576f44b7bc4567ae01
nahyraviana <nahyraviana@gmail.com>
2022-02-21 14:53:36 -0700

    Current Score Update

--- greed/game/directing/director.py
+++ greed/game/directing/director.py
@@ -1,6 +1,6 @@
 class Director:
-    """A person who directs the game. 
-    
+    """A person who directs the game.
+
     The responsibility of a Director is to control the sequence of play.
 
     Attributes:
@@ -10,14 +10,14 @@ class Director:
 
     def __init__(self, keyboard_service, video_service):
         """Constructs a new Director using the specified keyboard and video services.
-        
+
         Args:
             keyboard_service (KeyboardService): An instance of KeyboardService.
             video_service (VideoService): An instance of VideoService.
         """
         self._keyboard_service = keyboard_service
         self._video_service = video_service
-        
+
     def start_game(self, cast):
         """Starts the game using the given cast. Runs the main game loop.
 
@@ -33,13 +33,13 @@ class Director:
 
     def _get_inputs(self, cast):
         """Gets directional input from the keyboard and applies it to the robot.
-        
+
         Args:
             cast (Cast): The cast of actors.
         """
         robot = cast.get_first_actor("robots")
         velocity = self._keyboard_service.get_direction()
-        robot.set_velocity(velocity)        
+        robot.set_velocity(velocity)
 
     def _do_updates(self, cast):
         """Updates the robot's position and resolves any collisions with artifacts.
@@ -50,21 +50,23 @@ class Director:
         banner = cast.get_first_actor("banners")
         robot = cast.get_first_actor("robots")
         artifacts = cast.get_actors("artifacts")
+        current_score = 0
 
-        banner.set_text("")
+        banner.set_text(current_score)
         max_x = self._video_service.get_width()
         max_y = self._video_service.get_height()
         robot.move_next(max_x,max_y)
-                
+
         for artifact in artifacts:
             if robot.get_position().equals(artifact.get_position()):
                 message = artifact.get_message()
-                banner.set_text(message)    
+                banner.set_text(message)
                 #Put scoring here!
-        
+                current_score += 1
+
     def _do_outputs(self, cast):
         """Draws the actors on the screen.
-        
+
         Args:
             cast (Cast): The cast of actors.
         """
